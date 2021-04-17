bd329a904468804aadc0e7b60fa28ae8516c0952
Sruthika Baviriseaty <sbaviriseaty@ufl.edu>
2021-04-17 11:04:04 -0400

    menu changes

--- NaviGATOR/Movie.cpp
+++ NaviGATOR/Movie.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cmath>
 #include <queue>
 using namespace std;
 
@@ -79,31 +80,26 @@ public:
             recomendations--;
             n++;
         }
-        while (underflow > 0)
+        while (abs(underflow) > 0)
         {
             cout << n << ". Recommendation not made" << endl;
+            underflow++;
             n++;
         }
     }
 
     void addMovie(Movie *temp, string l, string g, int d, double r)
     {
-        if (listCapacity < INT_MAX)
-
-        {
-            makeMatch(temp, l, g, d, r);
-            list.emplace(temp);
-            listCapacity++;
-        }
-        else
-            cout << "List is full!" << endl;
+        makeMatch(temp, l, g, d, r);
+        list.emplace(temp);
+        listCapacity++;
     }
 };
 
 int main()
 {
     MovieNaviGATOR session;
-    //read movies in from csv 
+    //read movies in from csv
     //store movies in both unordered map and unordered set
     //ask user for preferences
     //
@@ -121,7 +117,7 @@ int main()
     while (n < 7 && n > 0)
     {
         cout << "Menu:" << endl;
-        cout << "1. Select preferred language (e.g. EN)" << endl;
+        cout << "1. Select preferred language (e.g. English)" << endl;
         cout << "2. Select preferred  genre (e.g. Action)" << endl;
         cout << "3. Select preferred duration in minutes (e.g. 120)" << endl;
         cout << "4. Select preferred main actor/actress (e.g. George Clooney)" << endl;
@@ -130,37 +126,41 @@ int main()
         cout << "7. Exit" << endl;
         cout << "Please enter a menu option (e.g. 3)" << endl;
         cin >> n;
-        switch(n) 
+        switch (n)
         {
-            case 1:
-                cout << "Enter preferred language (e.g. EN)" << endl;
-                cin >> language;
-                break;
-            case 2:
-                cout << "Enter preferred genre (e.g. Action)" << endl;
-                cin >> genre;
-                break;
-            case 3:
-                cout << "Enter preferred duration in minutes (e.g. 120)" << endl;
-                cin >> duration;
-                break;
-            case 4:
-                cout << "Enter preferred main actor/actress (e.g. George Clooney)" << endl;
-                cin >> actor;
-                break;
-            case 5:
-                cout << "Enter preferred ranking (e.g. 7.5)" << endl;
-                cin >> rating;
-                break;
-            case 6:
-                cout << "Enter number of recommendations" << endl;
-                cin >> number;
-                cout << "Your top " << number << " movies are:" << endl;
-                session.showpq(number);
-                break;
-            default:
-                break;
-        }  
-        cout << endl;      
+        case 1:
+            cout << "Languages in database: English, French, Spanish, Japanese, Italian, Hindi, German, Turkish, Russian, Korean, Portuguese, Malayalam, Tamil, Mandarin, Telugu, Cantonese, Persian, Swedish, Polish, Greek, Arabic, Danish, Dutch, Bengali, Finnish, Czech, Hungarian, Norwegian, Romanian, Thai" << endl;
+            cout << "Enter preferred language (e.g. English)" << endl;
+            cin >> language;
+            break;
+        case 2:
+            cout << "Genres in database: Action, Adventure, Biography, Comedy, Crime, Drama, Family, Fantasy, History, Horror, Music, Musical, Mystery, Romance, Sci-Fi, Thriller, War, Western" << endl;
+            cout << "Enter preferred genre (e.g. Action)" << endl;
+            cin >> genre;
+            break;
+        case 3:
+            cout << "Range of durations (in minutes) in database: 40 - 800" << endl;
+            cout << "Enter preferred duration in minutes (e.g. 120)" << endl;
+            cin >> duration;
+            break;
+        case 4:
+            cout << "Enter preferred main actor/actress (e.g. George Clooney)" << endl;
+            cin >> actor;
+            break;
+        case 5:
+            cout << "Range of rankings in database: 1 - 10" << endl;
+            cout << "Enter preferred ranking (e.g. 7.5)" << endl;
+            cin >> rating;
+            break;
+        case 6:
+            cout << "Enter number of recommendations" << endl;
+            cin >> number;
+            cout << "Your top " << number << " movies are:" << endl;
+            session.showpq(number);
+            break;
+        default:
+            break;
+        }
+        cout << endl;
     }
 }
